b7c5e853e293360f62704c5bfcf9c4c5f4faa5be
Thomas Broyer <t.broyer@ltgt.net>
2024-04-24 20:47:53 +0200

    Prepare library project for publishing

--- build-logic/src/main/kotlin/local.java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/local.java-conventions.gradle.kts
@@ -55,6 +55,28 @@ tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
+tasks.withType<Javadoc>().configureEach {
+    isFailOnError = true
+    (options as StandardJavadocDocletOptions).apply {
+        noTimestamp()
+        quiet()
+        use()
+        addStringOption("-release", java.sourceCompatibility.majorVersion)
+        addBooleanOption("Xdoclint:-missing", true)
+        addStringOption("-link-modularity-mismatch", "info")
+        links(
+            "https://jakarta.ee/specifications/servlet/6.0/apidocs/",
+            "https://checkerframework.org/api/",
+            "https://errorprone.info/api/latest/"
+        )
+        // Nimbus OIDC SDK javadoc doesn't include a package-list
+        linksOffline(
+            "https://javadoc.io/doc/com.nimbusds/oauth2-oidc-sdk/latest/",
+            rootProject.file("build-logic/src/javadoc-link/nimbusds-oidc-sdk/").toRelativeString(projectDir)
+        )
+    }
+}
+
 spotless {
     java {
         googleJavaFormat(project.versionCatalogs.named("libs").findVersion("googleJavaFormat").orElseThrow().requiredVersion)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     id("local.common-conventions")
+    alias(libs.plugins.nexusPublish)
+}
+
+nexusPublishing {
+    packageGroup = "net.ltgt.oidc"
+    useStaging = (version != Project.DEFAULT_VERSION)
+    repositories {
+        sonatype()
+    }
 }
