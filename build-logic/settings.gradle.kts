29ef920463ad0be433a30fb4d83f1bd819244453
Thomas Broyer <t.broyer@ltgt.net>
2024-10-25 17:00:19 +0200

    Gradle Plugin Portal no longer redirects to JCenter but to Central
    Note that build-logic's build needs Central for Spotless/Ktlint

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,20 +1,8 @@
 rootProject.name = "oidc-servlets-build-logic"
 
-pluginManagement {
-    repositories {
-        // gradlePluginPortal redirects to JCenter which is not reliable,
-        // prefer Central to JCenter (for the same dependencies)
-        // see https://github.com/gradle/gradle/issues/15406
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
-        // gradlePluginPortal redirects to JCenter which is not reliable,
-        // prefer Central to JCenter (for the same dependencies)
-        // see https://github.com/gradle/gradle/issues/15406
         mavenCentral()
         gradlePluginPortal()
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,6 @@
 rootProject.name = "oidc-servlets"
 
 pluginManagement {
-    repositories {
-        // gradlePluginPortal redirects to JCenter which is not reliable,
-        // prefer Central to JCenter (for the same dependencies)
-        // see https://github.com/gradle/gradle/issues/15406
-        mavenCentral()
-        gradlePluginPortal()
-    }
     includeBuild("build-logic")
 }
 
