5775acf7b0352ee3185abd01d9591d1d3abccfc2
Thomas Broyer <t.broyer@ltgt.net>
2025-05-21 15:26:39 +0200

    Move conditional 401 vs 403 up to AbstractAuthorizationFilter
    …to make it easier to implement other filters similar to HasRoleFilter.
    
    This is the same as 90514047a09633cb75435104f813154e1af41269 applied
    to oidc-servlets-rs.

--- rs/src/main/java/net/ltgt/oidc/servlet/rs/AbstractAuthorizationFilter.java
+++ rs/src/main/java/net/ltgt/oidc/servlet/rs/AbstractAuthorizationFilter.java
@@ -4,6 +4,7 @@ import com.google.errorprone.annotations.ForOverride;
 import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import jakarta.annotation.Priority;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -41,18 +42,32 @@ public abstract class AbstractAuthorizationFilter implements ContainerRequestFil
    * This method is called whenever the user is not authorized and the request is a {@linkplain
    * Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
    *
+   * <p>This implementation calls {@link #sendForbidden} whenever the user is authenticated, and
+   * defers to the {@link #doRedirectToAuthenticationEndpoint} otherwise.
+   */
+  @ForOverride
+  protected void redirectToAuthenticationEndpoint(ContainerRequestContext containerRequestContext) {
+    if (containerRequestContext.getSecurityContext().getUserPrincipal() == null) {
+      doRedirectToAuthenticationEndpoint(containerRequestContext);
+    } else {
+      sendForbidden(containerRequestContext);
+    }
+  }
+
+  /**
+   * This method is called whenever the user is not authorized and the request is a {@linkplain
+   * Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
+   *
    * <p>The default implementation simply calls the globally configured {@link
    * AuthenticationRedirector}, and allows {@linkplain #configureAuthenticationRequest configuring
    * the authentication request}.
    *
-   * <p>Subclasses can override this method to conditionally generate different responses.
-   *
    * @see #configureAuthenticationRequest
-   * @see HasRoleFilter
    * @see #sendUnauthorized
    */
   @ForOverride
-  protected void redirectToAuthenticationEndpoint(ContainerRequestContext containerRequestContext) {
+  protected void doRedirectToAuthenticationEndpoint(
+      ContainerRequestContext containerRequestContext) {
     var authenticationRedirector = getAuthenticationRedirector();
     authenticationRedirector.redirectToAuthenticationEndpoint(
         containerRequestContext,
@@ -88,20 +103,47 @@ public abstract class AbstractAuthorizationFilter implements ContainerRequestFil
       ContainerRequestContext containerRequestContext, AuthenticationRequest.Builder builder) {}
 
   /**
-   * This method is called whenever is not authorized and the request is <b>not</b> a {@linkplain
-   * Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
+   * This method is called whenever the user is not authorized and the request is <b>not</b> a
+   * {@linkplain Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
+   *
+   * <p>This implementation calls {@link #sendForbidden} whenever the user is authenticated, and
+   * defers to {@link #doSendUnauthorized} otherwise.
+   */
+  @ForOverride
+  protected void sendUnauthorized(ContainerRequestContext containerRequestContext) {
+    if (containerRequestContext.getSecurityContext().getUserPrincipal() == null) {
+      doSendUnauthorized(containerRequestContext);
+    } else {
+      sendForbidden(containerRequestContext);
+    }
+  }
+
+  /**
+   * This method is called whenever the user is not authorized and the request is <b>not</b> a
+   * {@linkplain Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
    *
    * <p>The default implementation simply throws a {@link NotAuthorizedException} without a {@code
    * WWW-Authenticate} response header. This is not strictly HTTP-compliant as it's missing the
    * {@code WWW-Authenticate} response header, but is a good way to signal the error to JavaScript
    * clients making an AJAX request.
    *
+   * @see #sendUnauthorized
    * @see #redirectToAuthenticationEndpoint
-   * @see HasRoleFilter
    */
   @ForOverride
-  protected void sendUnauthorized(ContainerRequestContext containerRequestContext) {
+  protected void doSendUnauthorized(ContainerRequestContext containerRequestContext) {
     // XXX: this is not http-compliant as it's missing WWW-Authenticate
     throw new NotAuthorizedException(Response.status(Response.Status.UNAUTHORIZED).build());
+  }
+
+  /**
+   * This method is called whenever the user is authenticated but not authorized.
+   *
+   * <p>The default implementation simply throws a {@link ForbiddenException}.
+   */
+  @ForOverride
+  protected void sendForbidden(
+      @SuppressWarnings("unused") ContainerRequestContext containerRequestContext) {
+    throw new ForbiddenException();
   }
 }
--- rs/src/main/java/net/ltgt/oidc/servlet/rs/HasRoleFilter.java
+++ rs/src/main/java/net/ltgt/oidc/servlet/rs/HasRoleFilter.java
@@ -2,21 +2,13 @@ package net.ltgt.oidc.servlet.rs;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.errorprone.annotations.ForOverride;
 import jakarta.annotation.Priority;
-import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.Priorities;
-import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.core.SecurityContext;
 import net.ltgt.oidc.servlet.UserPrincipal;
 
 /**
  * Ensures the user {@linkplain SecurityContext#isUserInRole has a given role}.
- *
- * <p>When the user is not authorized, the default implementation will return a {@link
- * ForbiddenException 403 Forbidden} error when the user is authenticated but is missing the
- * required role, and defers to the {@linkplain AbstractAuthorizationFilter parent behavior}
- * otherwise.
  *
  * <p>An instance of this class needs to be registered through a {@link
  * jakarta.ws.rs.container.DynamicFeature DynamicFeature}, or a subclass needs to be created to be
@@ -38,71 +30,5 @@ public class HasRoleFilter extends AbstractAuthorizationFilter {
     } else {
       return false;
     }
-  }
-
-  /**
-   * This method is called whenever the user is not authorized and the request is a {@linkplain
-   * Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
-   *
-   * <p>This implementation calls {@link #sendForbidden} whenever the user is authenticated, and
-   * defers to the {@linkplain AbstractAuthorizationFilter#redirectToAuthenticationEndpoint parent
-   * behavior} otherwise.
-   */
-  @ForOverride
-  @Override
-  protected void redirectToAuthenticationEndpoint(ContainerRequestContext containerRequestContext) {
-    if (containerRequestContext.getSecurityContext().getUserPrincipal() == null) {
-      doRedirectToAuthenticationEndpoint(containerRequestContext);
-    } else {
-      sendForbidden(containerRequestContext);
-    }
-  }
-
-  /**
-   * Calls {@link #redirectToAuthenticationEndpoint} from the superclass. This is a hook allowing to
-   * bypass this class' override's implementation.
-   */
-  @SuppressWarnings("ForOverride")
-  protected final void doRedirectToAuthenticationEndpoint(
-      ContainerRequestContext containerRequestContext) {
-    super.redirectToAuthenticationEndpoint(containerRequestContext);
-  }
-
-  /**
-   * This method is called whenever is not authorized and the request is <b>not</b> a {@linkplain
-   * Utils#isSafeMethod safe} {@linkplain Utils#isNavigation navigation} request.
-   *
-   * <p>This implementation calls {@link #sendForbidden} whenever the user is authenticated, and
-   * defers to the {@linkplain AbstractAuthorizationFilter#sendUnauthorized parent behavior}
-   * otherwise.
-   */
-  @ForOverride
-  @Override
-  protected void sendUnauthorized(ContainerRequestContext containerRequestContext) {
-    if (containerRequestContext.getSecurityContext().getUserPrincipal() == null) {
-      doSendUnauthorized(containerRequestContext);
-    } else {
-      sendForbidden(containerRequestContext);
-    }
-  }
-
-  /**
-   * Calls {@link #sendUnauthorized} from the superclass. This is a hook allowing to bypass this
-   * class' override's implementation.
-   */
-  @SuppressWarnings("ForOverride")
-  protected final void doSendUnauthorized(ContainerRequestContext containerRequestContext) {
-    super.sendUnauthorized(containerRequestContext);
-  }
-
-  /**
-   * This method is called whenever the user is authenticated but not authorized.
-   *
-   * <p>The default implementation simply throws a {@link ForbiddenException}.
-   */
-  @ForOverride
-  protected void sendForbidden(
-      @SuppressWarnings("unused") ContainerRequestContext containerRequestContext) {
-    throw new ForbiddenException();
   }
 }
