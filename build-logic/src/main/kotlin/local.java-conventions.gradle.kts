d0f505b126a2ac18e2647049b19aa148cb1e3fbe
Thomas Broyer <t.broyer@ltgt.net>
2026-02-16 12:56:18 +0100

    Add license header to source files

--- build-logic/src/main/kotlin/local.java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/local.java-conventions.gradle.kts
@@ -108,5 +108,6 @@ tasks.withType<Javadoc>().configureEach {
 spotless {
     java {
         googleJavaFormat(project.versionCatalogs.named("libs").findVersion("googleJavaFormat").orElseThrow().requiredVersion)
+        licenseHeaderFile(rootProject.file("LICENSE.header"))
     }
 }
